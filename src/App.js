ffbfbf4666fa901bd1962060586d91922b283df2
rogerio rossa <rogeriorossa@gmail.com>
2019-05-06 20:26:46 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle>
+        <div className="App">
+          <h1>Hello World</h1>
+        </div>
+      </GlobalStyle>
+    </Fragment>
+  );
+}
+
+export default App;
