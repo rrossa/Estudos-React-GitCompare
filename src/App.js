85643dbd35af9d47aac2cd5c845a6b9646cdafad
rogerio rossa <rogeriorossa@gmail.com>
2019-05-07 16:50:57 -0300

    modificações finais

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react';
 import GlobalStyle from './styles/global';
 
-function App() {
-  return (
-    <Fragment>
-      <GlobalStyle>
-        <div className="App">
-          <h1>Hello World</h1>
-        </div>
-      </GlobalStyle>
-    </Fragment>
-  );
-}
+import Main from './pages/Main';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Main />
+  </Fragment>
+);
 
 export default App;
